25f50e0b647910324b3bd56c7dba973cdcfb818e
Gowtham Kumar <gowthamkumar20200@gmail.com>
2021-08-29 22:22:03 +0530

    Recipie app created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, RecipiesComponent, RecipeListComponent, RecipeDetailComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/recipies/recipies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipies',
+  templateUrl: './recipies.component.html',
+  styleUrls: ['./recipies.component.css']
+})
+export class RecipiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
